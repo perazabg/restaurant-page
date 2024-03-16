3a638a11fc8675a55777e2cedd8c70af0d41771d
perazabg <96875802+perazabg@users.noreply.github.com>
2024-03-16 16:22:06 -0700

    hardcoded html dist file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  
   entry: "./src/index.js",
   output: {
     filename: "main.js",
@@ -13,6 +12,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
