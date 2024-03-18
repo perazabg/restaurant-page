66f288d8966ccb3e8aef15398213f9b31d002ad5
perazabg <96875802+perazabg@users.noreply.github.com>
2024-03-17 20:14:24 -0700

    added the css file to src folder

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+import './style.css';
+
+const content = document.getElementById('content');
+const homeBtn = document.getElementById('home');
+const menuBtn = document.getElementById('menu');
+const aboutBtn = document.getElementById('about');
+
